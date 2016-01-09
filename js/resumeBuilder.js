a9acf7915a30c4ecd7a22ec47a3f538f6c8202ec
Gabriel Mojica <niko.mojica@live.com>
2016-01-10 03:30:12 +1100

    add name and role back to header"

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -10,44 +10,44 @@ var bio = {
 		"location": "Sydney, Australia"
 	},
 	"welcomeMessage": "Welcome to my portfolio",
-	"skills": "Javascript, HTML5, CSS3, SASS, Git, Java, Bash",
+	"skills": ["Javascript", "HTML5", "CSS3", "Sass", "Git", "Java", "Bash"],
 	"biopic": "What is this?",
-	"display": function(){}
+	"display": function() {}
 };
 
 var work = {
-	"job": [
+	"jobs": [
 		{
 			"employer": "ParkTrent Properties Group",
 			"title": "Face-to-Face Promotions",
 			"location": "Sydney, Australia",
-			"dates": "2012-9",
+			"dates": "2012-9-10",
 			"description": "Promoting a real estate seminar"
 		}
 	]
 };
 
 var education = {
-    "schools": 
-    [
-    	{
-    		"name": "Univesity of Technology, Sydney",
-    		"location": "Sydney, Australia",
-    		"degree": "Bachelour of Science in Information Technology, Diploma of Professional Practice",
-    		"Majors": ["Enterprise Systems Development"],
-    		"dates": "2014-28-3",
-    		"url": "http://handbook.uts.edu.au/courses/c10345.html"
-    	}
-    ],
-    "onlineCourses": 
-    [
-    	{
-    		"title": "Front-end Developer",
-    		"school": "Udacity",
-    		"date": "2016-1-5",
-    		"url": "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
-    	}
-    ]
+	"schools":
+		[
+			{
+				"name": "Univesity of Technology, Sydney",
+				"location": "Sydney, Australia",
+				"degree": "Bachelour of Science in Information Technology, Diploma of Professional Practice",
+				"Majors": ["Enterprise Systems Development"],
+				"dates": "2014-28-3",
+				"url": "http://handbook.uts.edu.au/courses/c10345.html"
+		}
+	],
+	"onlineCourses":
+	[
+		{
+			"title": "Front-end Developer",
+			"school": "Udacity",
+			"date": "2016-1-5",
+			"url": "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
+		}
+	]
 };
 
 var projects = {
@@ -61,6 +61,32 @@ var projects = {
 		}
 	]
 };
+
+
+var name = HTMLheaderName.replace("%data%", bio.name);
+var role = HTMLheaderRole.replace("%data%", bio.role);
+
+$("#header").append(name);
+$("#header").append(role);
+
+
+if (bio.skills.length > 0) {
+	$("#header").append(HTMLskillsStart);
+
+	bio.skills.forEach(function(skill) {
+		$("#skills").append(HTMLskills.replace("%data%", skill));
+	});
+}
+
+if (work.jobs.length > 0) {
+	$("#workExperience").append(HTMLworkStart);
+	work.jobs.forEach(function(job) {
+		$("")
+	});
+}
+
+
+
 
 
 
