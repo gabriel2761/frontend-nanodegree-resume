35ffccd83caa5d5cc55af050563029eef32222f1
Gabriel Mojica <niko.mojica@live.com>
2016-01-10 17:46:27 +1100

    add projects to list

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,7 @@
 
 
 var bio = {
-	"name": "Gabriel",
+	"name": "Gabriel Mojica",
 	"role": "Student",
 	"contacts" : {
 		"mobile": "04 3529 xxxx",
@@ -12,19 +12,12 @@ var bio = {
 	"welcomeMessage": "Welcome to my portfolio",
 	"skills": ["Javascript", "HTML5", "CSS3", "Sass", "Bootstrap", "JQuery", "Git", "Polymer", "Java", "Bash"],
 	"biopic": "What is this?",
-	"display": displayWork
+	"display": function() { }
 };
 
 var work = {
-	"jobs": 
+	"jobs":
 	[
-		{
-			"employer": "ParkTrent Properties Group",
-			"title": "Face-to-Face Promotions",
-			"location": "Sydney, Australia",
-			"dates": "2012-9-10",
-			"description": "Promoting a real estate seminar"
-		},
 		{
 			"employer": "ParkTrent Properties Group",
 			"title": "Face-to-Face Promotions",
@@ -64,7 +57,7 @@ var projects = {
 			"title": "Front-end Portfolio",
 			"dates": "2016-1-7",
 			"description": "Create a page to feature web development projects",
-			"images": ["http://placehold.it/350x150"],
+			"image": ["http://placehold.it/60x60"],
 			"display": function(){}
 		}
 	]
@@ -78,7 +71,7 @@ $("#header").append(role);
 
 
 if (bio.skills.length > 0) {
-	
+
 	$("#header").append(HTMLskillsStart);
 
 	bio.skills.forEach(function(skill) {
@@ -86,12 +79,11 @@ if (bio.skills.length > 0) {
 	});
 }
 
-
-
+$("#hello-daddy-pig")
 
 var displayWork = function() {
 	if (work.jobs.length > 0) {
-	
+
 		work.jobs.forEach(function(job) {
 
 			var employer = HTMLworkEmployer.replace("%data%", job.employer);
@@ -110,14 +102,28 @@ var displayWork = function() {
 	}
 }
 
-bio.display
+displayWork();
 
+$(document).click(function(loc) {
+	logClicks(loc.pageX, loc.pageY);
+});
 
+projects.display = function() {
 
+	this.projects.forEach(function(project) {
 
+		var title = HTMLprojectTitle.replace("%data%", project.title);
+		var date = HTMLprojectDates.replace("%data%", project.dates);
+		var description = HTMLprojectDescription.replace("%data%", project.description);
+		var image = HTMLprojectImage.replace("%data%", project.image);
 
+		$("#projects").append(HTMLprojectStart);
+		$(".project-entry").append(title);
+		$(".project-entry").append(date);
+		$(".project-entry").append(description);
+		$(".project-entry").append(image);
 
+	});
+}
 
-
-
-
+projects.display();
