951e80e4f1b992883d2a4cb79fa996f0a2e857b7
Gabriel Mojica <niko.mojica@live.com>
2016-01-10 22:42:17 +1100

    feat: add display functions to all JSON objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -11,7 +11,31 @@ var bio = {
 	"welcomeMessage": "Welcome to my portfolio",
 	"skills": ["Javascript", "HTML5", "CSS3", "Sass", "Bootstrap", "JQuery", "Git", "Polymer", "Php", "Java", "Bash"],
 	"biopic": "What is this?",
-	"display": function() { }
+	"display": function
+};
+
+var education = {
+	"schools":
+	[
+		{
+			"name": "Univesity of Technology, Sydney",
+			"location": "Sydney, Australia",
+			"degree": "Bachelour of Science in Information Technology, Diploma of Professional Practice",
+			"majors": ["Enterprise Systems Development"],
+			"dates": "2014-28-3 - present",
+			"url": "http://handbook.uts.edu.au/courses/c10345.html"
+		}
+	],
+	"onlineCourses":
+	[
+		{
+			"title": "Front-end Developer",
+			"school": "Udacity",
+			"date": "2016-1-5 - present",
+			"url": "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
+		}
+	],
+	"display": function
 };
 
 var work = {
@@ -24,31 +48,10 @@ var work = {
 			"dates": "2012-9-10",
 			"description": "Promoting a real estate seminar"
 		}
-	]
+	],
+	"display": function
 };
 
-var education = {
-	"schools":
-		[
-			{
-				"name": "Univesity of Technology, Sydney",
-				"location": "Sydney, Australia",
-				"degree": "Bachelour of Science in Information Technology, Diploma of Professional Practice",
-				"Majors": ["Enterprise Systems Development"],
-				"dates": "2014-28-3",
-				"url": "http://handbook.uts.edu.au/courses/c10345.html"
-		}
-	],
-	"onlineCourses":
-	[
-		{
-			"title": "Front-end Developer",
-			"school": "Udacity",
-			"date": "2016-1-5",
-			"url": "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
-		}
-	]
-};
 
 var projects = {
 	"projects": [
@@ -57,7 +60,7 @@ var projects = {
 			"dates": "2016-1-7",
 			"description": "Create a page to feature web development projects",
 			"image": ["http://placehold.it/60x60"],
-			"display": function(){}
+			"display": function;
 		}
 	]
 };
